b464af7d16d8e306373492328fbfef6bad88eb47
Freericks <webdev123487@outlook.com>
2024-05-10 23:21:37 -0400

    Set Up run.py

--- run.py
+++ run.py
@@ -3,3 +3,7 @@ import bottle
 @bottle.route('/')
 def index():
      return bottle.template('index')
+@bottle.route('/<name>')
+def message(name):
+    the_message = f'Hello {name}!'
+    return bottle.template('message_template', message=the_message)
